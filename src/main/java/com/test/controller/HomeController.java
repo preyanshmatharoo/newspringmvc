7a9926c9a14569e776dafa8194e5ba0d801870ef
Preyansh Singh <Preyansh Singh>
2017-02-17 00:16:11 +0530

    First Commit

---
+++ src/main/java/com/test/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.test.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@EnableAutoConfiguration
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView home(){
+		return new ModelAndView("home");
+	}
+
+}
